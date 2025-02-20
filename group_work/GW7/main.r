9312d114db3fd1d2a48605bbe876753f32a4fb9d
pranav-jay26 <p_jayakumar@u.pacific.edu>
2025-02-19 23:18:38 -0800

    Add stuff

---
+++ group_work/GW7/main.r
@@ -0,0 +1,5 @@
+#!/usr/bin/env Rscript
+
+data <- read.csv("../../data/framingham.csv")
+
+
--- module_assessments/MA2/main.r
+++ module_assessments/MA2/main.r
@@ -1,6 +1,3 @@
 #!/usr/bin/env Rscript
 
-data <- read.csv("../../data/election2016.csv")
-colnames(data)
-
-trump_lm
+trump_lm <- lm()
